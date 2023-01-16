e28cb6a7b019483b163f1fdc34f71080fde4c400
Ivan Shtarev <ishtarev@outlook.com>
2023-01-16 22:23:52 +0500

    continue working on code

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ SIZE = (1280, 720)
 FPS = 30
 G = 1
 
-
 class Background(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
@@ -22,8 +21,13 @@ def terminate():
     sys.exit()
 
 
+pygame.init()
+size = width, height = SIZE
+screen = pygame.display.set_mode(size)
+
+
 def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join('assets', name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"File named '{fullname}' is not found")
@@ -40,19 +44,27 @@ def load_image(name, colorkey=None):
 
 
 class Fruit(pygame.sprite.Sprite):
-    def __init__(self, *group, image_name):
-        super().__init__(*group)
-        path = os.path.join("assets", image_name + '.png')
-        self.image = pygame.image.load(path)
+    # image = load_image("assets/apple_full.png")
+    image = load_image("apple_full.png")
+    # image_boom = load_image("apple_half.png")
+    image_boom = load_image("apple_half.png")
+
+    def __init__(self, group):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite. 
+        # Это очень важно !!!
+        super().__init__(group)
+        self.image = Fruit.image
         self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(1000)
-        self.rect.y = random.randrange(500)
+        self.rect.x = random.randrange(SIZE[0])
+        self.rect.y = random.randrange(SIZE[1])
+
     def update(self, *args):
         self.rect = self.rect.move(random.randrange(3) - 1, 
                                    random.randrange(3) - 1)
         if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
                 self.rect.collidepoint(args[0].pos):
             self.image = self.image_boom
+
 
 
 
@@ -66,16 +78,18 @@ def main():
     screen.blit(BackGround.image, BackGround.rect)
 
     all_sprites = pygame.sprite.Group()
-    Fruit(all_sprites, image_name='apple_full')
+    for _ in range(50):
+        Fruit(all_sprites)
+    all_sprites.draw(screen)
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
+         
 
-        all_sprites.draw(screen)
-        all_sprites.update()
+        all_sprites.update(event)
 
         # screen.blit(BackGround.image, BackGround.rect)
         pygame.display.update()
